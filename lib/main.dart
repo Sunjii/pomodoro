bd8cb1195de950073a473ed10ad9deb495c02cd1
Sunjii <sophieadenn@gmail.com>
2023-02-06 15:45:30 +0900

    Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,22 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('init!!');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return MaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
